6b252287cfb8d3bd8fe2196b0e92c821a98fa65b
defendimattia <defendi.mattia92@gmail.com>
2025-03-05 15:31:42 +0100

    add: module 1 completed

--- js/main.js
+++ js/main.js
@@ -2,13 +2,36 @@ const elementInputKm = document.getElementById("inputKm")
 const elementInputAge = document.getElementById("inputAge")
 const form = document.querySelector("form")
 
-let valInputKm = 0
-let valInputAge = 0
+
+
+
 
 form.addEventListener("submit", (event) => {
 
     event.preventDefault()
-    valInputKm = parseInt(elementInputKm.value)
-    valInputAge = parseInt(elementInputAge.value)
+    let valInputKm = parseInt(elementInputKm.value)
+    let valInputAge = parseInt(elementInputAge.value)
 
+    const ticketPrice = valInputKm * 0.21
+    let Discount = 0
+
+
+    //     - se minorenne (età < 18)
+    if (valInputAge < 18) {
+        //         - sconto 20%
+        Discount = (ticketPrice * 20) / 100
+        //     - se età over 65 (età > 65)  
+    } else if (valInputAge > 65) {
+        //         - sconto 40%
+        Discount = (ticketPrice * 40) / 100
+    }
+    //     - altrimenti prezzo pieno
+
+    // - il prezzo deve essere printato in forma umana (massimo 2 decimali)
+    console.log((ticketPrice - Discount).toFixed(2))
 })
+
+
+
+
+
